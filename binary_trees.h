4e1ead354454c123b806bff72a87d2d14767048f
Amiir25 <amirsadik636@gmail.com>
2024-12-05 14:33:45 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -8,7 +8,7 @@
 /* Data Structure */
 
 /**
- * struct binary_trees_s - Binary tree node
+ * struct binary_tree_s - Binary tree node
  *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
@@ -22,11 +22,19 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
 
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
 
 /* Prototypes */
-
 binary_tree_t *binary_tree_node(binary_tree_t *, int);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *, int);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *, int);
