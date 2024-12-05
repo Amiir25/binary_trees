cea75ae922ea3a63f2cc4b9e3577e1a7f3619d10
Amiir25 <amirsadik636@gmail.com>
2024-12-05 09:39:49 +0300

    Add 101-binary_tree_levelorder.c

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - Goes through a binary tree using
+ * levelorder traversal
+ * @tree: A pointer to the root node of the tree
+ * @func: A pointer to a function to call for each node
+ *
+ * Return: Nothing
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	func(tree->n);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
